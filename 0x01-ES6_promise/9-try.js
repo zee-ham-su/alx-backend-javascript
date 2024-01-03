932437e13977a39726385e52b6ef19a7976267eb
zee-ham-su <hsufiian@yahoo.com>
2024-01-03 14:47:34 +0000

    100-await

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,6 +1,5 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-
   try {
     const result = mathFunction();
     queue.push(result);
@@ -9,6 +8,5 @@ export default function guardrail(mathFunction) {
   } finally {
     queue.push('Guardrail was processed');
   }
-
   return queue;
 }
