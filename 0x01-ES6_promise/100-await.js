0c965fb0d296f627d8350f311027ed619ad7972f
zee-ham-su <hsufiian@yahoo.com>
2024-01-03 14:50:47 +0000

    100 await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    try {
-        const photoPromise = await uploadPhoto();
-        const userPromise = await createUser();
+  try {
+    const photoPromise = await uploadPhoto();
+    const userPromise = await createUser();
 
-        return ({ photo: photoPromise, user: userPromise });
-    } catch (error) {
-        return ({ photo: null, user: null });
-    }
+    return ({ photo: photoPromise, user: userPromise });
+  } catch (error) {
+    return ({ photo: null, user: null });
+  }
 }
