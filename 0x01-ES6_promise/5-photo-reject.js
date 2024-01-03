525ea9d8ea71d16b982fae12c1c38ddb2db7a786
zee-ham-su <hsufiian@yahoo.com>
2024-01-03 10:16:28 +0000

    photo reject

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,3 @@
 export default function uploadPhoto(fileName) {
-  const errorMessage = `$${fileName} cannot be processed`;
-  return Promise.reject(new Error(errorMessage));
+  return Promise.reject(Error(`${fileName} cannot be processed`));
 }
