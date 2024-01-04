a587cbb20c604ee8d5abc1886c6db722ed072635
zee-ham-su <hsufiian@yahoo.com>
2024-01-04 13:06:48 +0000

    airpot

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,25 +3,14 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = SkyHighBuilding.validateNumber(floors, 'floors');
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(newFloors) {
-    this._floors = SkyHighBuilding.validateNumber(newFloors, 'floors');
-  }
-
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors.`;
-  }
-
-  static validateNumber(value, attributeName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attributeName} must be a number.`);
-    }
-    return value;
   }
 }
