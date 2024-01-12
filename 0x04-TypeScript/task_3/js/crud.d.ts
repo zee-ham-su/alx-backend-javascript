3ebd7bdcc028b0f246d0d77285232d58ee7d7883
zee-ham-su <hsufiian@yahoo.com>
2024-01-12 17:08:23 +0000

    correction

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,5 +1,5 @@
 import { RowID, RowElement } from './interface';
 
-declare function insertRow(row: RowElement): number;
-declare function deleteRow(rowId: RowID): undefined;
-declare function updateRow(rowId: RowID, row: RowElement): number;
+export declare function insertRow(row: RowElement): number;
+export declare function deleteRow(rowId: RowID): void;
+export declare function updateRow(rowId: RowID, row: RowElement): number;
