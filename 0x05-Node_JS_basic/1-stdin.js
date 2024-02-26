4a24bce010c83d8df8f4b6656a1550e04e6f56bd
zee-ham-su <hsufiian@yahoo.com>
2024-02-26 19:14:30 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,16 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-readline.question(`Welcome to Holberton School, what is your name?\n`, name => {
-  console.log(`Your name is: ${name}`);
-  readline.close();
-});
-
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
