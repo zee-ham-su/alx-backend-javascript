761d29fce4cde8152eee813554c3670524057cc1
zee-ham-su <hsufiian@yahoo.com>
2024-02-26 19:36:52 +0000

    Refactor HTTP server to display student data***

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,28 +1,21 @@
 const http = require('http');
-const { readDatabase } = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
   if (req.url === '/') {
-    res.statusCode = 200;
-    res.end('Hello Holberton School!\n');
-  } else if (req.url === '/students') {
-    readDatabase(process.argv[2], (error, data) => {
-      if (error) {
-        res.statusCode = 500;
-        res.end('Cannot load the database\n');
-      } else {
-        res.statusCode = 200;
-        res.end(`This is the list of our students\n${data}`);
-      }
-    });
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found\n');
+    res.end('Hello Holberton School!');
   }
-});
-
-app.listen(1245);
+  if (req.url === '/students') {
+    countStudents(process.argv[2]).then((data) => {
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${data.students.length}\n`);
+      res.write(`Number of students in CS: ${data.count.CS}. List: ${data.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${data.count.SWE}. List: ${data.sweStudents.join(', ')}`);
+      res.end();
+    }).catch((err) => res.end(err.message));
+  }
+}).listen(1245);
 
 module.exports = app;
