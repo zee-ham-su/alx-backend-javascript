c143c5b5320578a3c90291d4c5af9df516fc1610
zee-ham-su <hsufiian@yahoo.com>
2024-02-26 19:35:10 +0000

    complex http correction

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,43 +1,28 @@
 const http = require('http');
-const fs = require('fs');
-
-function displayStudents(res, databasePath) {
-  try {
-    const data = fs.readFileSync(databasePath, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const headers = lines[0].split(',');
-    const students = lines.slice(1);
-
-    res.write('This is the list of our students:\n');
-
-    headers.forEach((header) => {
-      const fieldIndex = headers.indexOf(header);
-      const fieldStudents = students.map((student) => student.split(',')[fieldIndex]);
-      const uniqueFieldStudents = [...new Set(fieldStudents)];
-      res.write(`Number of students in ${header}: ${uniqueFieldStudents.length}. List: ${uniqueFieldStudents.join(', ')}\n`);
-    });
-
-    res.end();
-  } catch (error) {
-    res.end('Cannot load the database\n');
-  }
-}
+const { readDatabase } = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
+    res.statusCode = 200;
     res.end('Hello Holberton School!\n');
   } else if (req.url === '/students') {
-    const databasePath = 'database.csv';
-    displayStudents(res, databasePath);
+    readDatabase(process.argv[2], (error, data) => {
+      if (error) {
+        res.statusCode = 500;
+        res.end('Cannot load the database\n');
+      } else {
+        res.statusCode = 200;
+        res.end(`This is the list of our students\n${data}`);
+      }
+    });
   } else {
-    res.end('404 Not Found\n');
+    res.statusCode = 404;
+    res.end('Not Found\n');
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
