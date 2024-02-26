e2d0fea8956d44a97035faa7e82019e2b8765a72
zee-ham-su <hsufiian@yahoo.com>
2024-02-26 12:05:03 +0000

    process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
-const interface = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+const readlineInterface = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
-interface.on('line', (input) => {
-    console.log(`Your name is: ${input}`);
+readlineInterface.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
 });
 
-interface.on('close', () => {
-    console.log('This important software is now closing');
+readlineInterface.on('close', () => {
+  console.log('This important software is now closing');
 });
