cc498995b014b86e8a65fe051369341dc98b9da7
zee-ham-su <hsufiian@yahoo.com>
2024-02-28 11:06:10 +0000

    correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,17 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const input = data.trim();
+  if (input.length === 0) {
+    process.stderr.write('No input received. Please provide your name.\n');
+  } else {
+    process.stdout.write(`Your name is: ${input}\n`);
+  }
+  process.exit();
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('No more data received.\n');
+});
