0514c88fb023cc1e4f335c52ddc10af670f0728b
zee-ham-su <hsufiian@yahoo.com>
2024-02-28 20:45:18 +0000

    Combining descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,28 +1,19 @@
-const assert = require("assert");
-const calculateNumber = require("./1-calcul");
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+const { describe, it } = require('node:test');
 
-describe("calculateNumber()", function () {
-    it(`should return the sum of two rounded numbers`, function () {
-        assert.strictEqual(calculateNumber("SUM", 1, 2), 3);
+describe('calculateNumber', function() {
+    it('sum of two round numbers', () => {
+        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
     });
-
-    it(`should return the result of subtracting b from a`, function () {
-        assert.strictEqual(calculateNumber("SUBTRACT", 1.4, 2.2), -1);
+    it('subtract two round numbers', () => {
+        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     });
-
-    it(`should return the result of subtracting b from a`, function () {
-        assert.strictEqual(calculateNumber("SUBTRACT", 4.9, 2.7), 2);
+    it('divide two round numbers', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     });
-
-    it(`should return the result of dividing a by b when b is not rounded to 0`, function () {
-        assert.strictEqual(calculateNumber("DIVIDE", 4, 2), 2);
-    });
-
-    it(`should return "Error" when b is rounded to 0`, function () {
-        assert.strictEqual(calculateNumber("DIVIDE", 1.7, 0), "Error");
-    });
-
-    it(`should return the result of dividing a by b when b is not rounded to 0`, function () {
-        assert.strictEqual(calculateNumber("DIVIDE", 1.4, 4.6), 0.2);
+    it('divide by zero', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
 });
+
