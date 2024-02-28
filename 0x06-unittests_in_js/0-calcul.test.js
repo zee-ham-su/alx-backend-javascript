461121002588a07537d8a4a2d1984d58b763bf75
zee-ham-su <hsufiian@yahoo.com>
2024-02-28 13:08:16 +0000

    mocha test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,21 +3,21 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
     it('checking if a and b have a rounded sum', () => {
-        assert.strictEqual(calculateNumber(2, 4), 6);
-        assert.strictEqual(calculateNumber(1, 5.8), 7);
-        assert.strictEqual(calculateNumber(2.3, 5.8), 8);
-        assert.strictEqual(calculateNumber(2.5, 5.8), 9);
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 
     it('checking if a is rounded and b is not rounded', () => {
-        assert.strictEqual(calculateNumber(1.3, 5), 6);
-        assert.strictEqual(calculateNumber(1.6, 5), 7);
+        assert.strictEqual(calculateNumber(2.3, 5), 7);
+        assert.strictEqual(calculateNumber(2.6, 5), 8);
         assert.strictEqual(calculateNumber(1.6, 5.8), 8);
     });
 
     it('checking if a is not rounded and b is rounded', () => {
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
-        assert.strictEqual(calculateNumber(1, 3.5), 5);
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1, 4.7), 6);
+        assert.strictEqual(calculateNumber(1, 6.5), 8);
+        assert.strictEqual(calculateNumber(1.1, 3.5), 5);
     });
 });
