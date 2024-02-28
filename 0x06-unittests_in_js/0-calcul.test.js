d9d1c5305a425ebf5a6e9f1226c0cd910ba0fb96
zee-ham-su <hsufiian@yahoo.com>
2024-02-28 19:07:42 +0000

    Refactor calculateNumber tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -10,17 +10,17 @@ describe('calculateNumber', () => {
   });
 
   it('checking if a is rounded and b is not rounded', () => {
-    assert.strictEqual(calculateNumber(2.3, 5), 7);
-    assert.strictEqual(calculateNumber(2.6, 5), 8);
-    assert.strictEqual(calculateNumber(1.6, 5.8), 8);
-    assert.strictEqual(calculateNumber(1.5, 4.8), 7);
-  });
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  })
+    ;
 
   it('checking if a is not rounded and b is rounded', () => {
-    assert.strictEqual(calculateNumber(1, 4.7), 6);
-    assert.strictEqual(calculateNumber(1, 6.5), 8);
-    assert.strictEqual(calculateNumber(1.1, 3.5), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.5), 5);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  })
   });
-
-});
