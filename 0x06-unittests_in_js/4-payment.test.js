1c7fc9e6498ecfd69230c013d90a8934e1859367
zee-ham-su <hsufiian@yahoo.com>
2024-02-29 13:36:08 +0000

    stub correction

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,13 +6,13 @@ const sendPaymentRequestToApi = require("./4-payment.js");
 
 describe("sendPaymentRequestToApi", function () {
     it("should stub Utils.calculateNumber and log the correct message", function () {
-        const calculateNumberStub = sinon.stub(Utils, "calculateNumber").returns(10);
+        const stub = sinon.stub(Utils, "calculateNumber").returns(10);
 
-        const consoleSpy = sinon.spy(console, "log");
+        const spy = sinon.spy(console, "log");
 
         sendPaymentRequestToApi(100, 20);
 
-        assert(consoleSpy.withArgs("The total is: 10").calledOnce);
-        assert(calculateNumberStub.withArgs("SUM", 100, 20).calledOnce);
+        assert(spy.withArgs("The total is: 10").calledOnce);
+        assert(stub.withArgs("SUM", 100, 20).calledOnce);
     });
 });
